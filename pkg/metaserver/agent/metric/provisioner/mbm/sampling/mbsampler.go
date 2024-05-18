2341ccc325e1b436044b436537d872f3c5ec6d03
Hongwei Chen <hongwei.chen@bytedance.com>
2024-05-18 04:49:26 +0000

    mbsampler: hooks up with mbw monitor, primely

--- pkg/metaserver/agent/metric/provisioner/mbm/sampling/mbsampler.go
+++ pkg/metaserver/agent/metric/provisioner/mbm/sampling/mbsampler.go
@@ -14,21 +14,48 @@ type MBSampler interface {
 
 type MBSamplerFactory func(metricConf *machine.KatalystMachineInfo) MBSampler
 
-type mbSampler struct{}
+type mbSampler struct {
+	monitor *MBMonitor
+}
 
-func (m mbSampler) Shutdown() {
+func (m *mbSampler) Shutdown() {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (m mbSampler) Init() {
+func (m *mbSampler) Init() {
+	if err := m.monitor.Init(); err != nil {
+		// todo: log error
+		// to set no-op monitor to avoid malfunctions
+		m.monitor = nil
+	}
 }
 
-func (m mbSampler) Sample(ctx context.Context) {
-	//TODO implement me
-	panic("implement me")
+func (m *mbSampler) Sample(ctx context.Context) {
+	if m.monitor == nil {
+		return
+	}
+
+	if err := m.monitor.ServePackageMB(); err == nil {
+		// todo: save to metrics store
+		//m.monitor.MemoryBandwidth.Packages
+	}
+
+	if err := m.monitor.ServeCoreMB(); err == nil {
+		// todo: save fake numa stats
+		// m.monitor.MemoryBandwidth.Numas
+	}
 }
 
 func NewMBSampler(metricConf *machine.KatalystMachineInfo) MBSampler {
-	return &mbSampler{}
+	monitor, err := NewMonitor()
+	if err != nil {
+		// todo: log error
+		// not to crash as mb monitor is not the most essential; fine keeping whole machine functioning with reduced capacity
+		return nil
+	}
+
+	return &mbSampler{
+		monitor: monitor,
+	}
 }
