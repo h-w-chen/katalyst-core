0cef3ea3bb14470ef458137a39b14498a1f9e083
Hongwei Chen <hongwei.chen@bytedance.com>
2024-09-27 03:55:35 +0000

    wip: mbm-poc varying shared qos subgroup support

--- pkg/agent/qrm-plugins/mb/controller/policy/qospolicy/weighted_qos_mb_policy_test.go
+++ pkg/agent/qrm-plugins/mb/controller/policy/qospolicy/weighted_qos_mb_policy_test.go
@@ -63,7 +63,7 @@ func Test_weightedQoSMBPolicy_GetPlan(t *testing.T) {
 	}
 }
 
-func Test_weightedQoSMBPolicy_getProportionalPlan(t *testing.T) {
+func Test_weightedQoSMBPolicy_getProportionalPlanToEnlarge(t *testing.T) {
 	t.Parallel()
 	type fields struct {
 		isTopLink bool
@@ -95,13 +95,37 @@ func Test_weightedQoSMBPolicy_getProportionalPlan(t *testing.T) {
 				"bar": {1: 200, 2: 100, 3: 100},
 			}},
 		},
+		{
+			name: "happy path of inside sub groups",
+			fields: fields{
+				isTopLink: false,
+			},
+			args: args{
+				totalMB: 1000,
+				currQoSMB: map[task.QoSLevel]*monitor.MBQoSGroup{
+					"foo": {
+						CCDMB: map[int]int{1: 100, 2: 200},
+						WeightedCCDMBs: map[int]map[int]int{
+							50: {1: 50, 2: 100},
+							30: {1: 50, 2: 100},
+						},
+					},
+					"bar": {CCDMB: map[int]int{1: 100, 2: 50, 3: 50}},
+				},
+			},
+			want: &plan.MBAlloc{Plan: map[task.QoSLevel]map[int]int{
+				"foo_50": {1: 100, 2: 200},
+				"foo_30": {1: 100, 2: 200},
+				"bar":    {1: 200, 2: 100, 3: 100},
+			}},
+		},
 	}
 	for _, tt := range tests {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 			w := &weightedQoSMBPolicy{}
-			assert.Equalf(t, tt.want, w.getProportionalPlan(tt.args.totalMB, tt.args.currQoSMB), "getProportionalPlan(%v, %v)", tt.args.totalMB, tt.args.currQoSMB)
+			assert.Equalf(t, tt.want, w.getProportionalPlanToEnlarge(tt.args.totalMB, tt.args.currQoSMB), "getProportionalPlan(%v, %v)", tt.args.totalMB, tt.args.currQoSMB)
 		})
 	}
 }
@@ -139,6 +163,52 @@ func Test_weightedQoSMBPolicy_getTopLevelPlan(t *testing.T) {
 			t.Parallel()
 			w := &weightedQoSMBPolicy{}
 			assert.Equalf(t, tt.want, w.getTopLevelPlan(tt.args.totalMB, tt.args.currQoSMB), "getTopLevelPlan(%v, %v)", tt.args.totalMB, tt.args.currQoSMB)
+		})
+	}
+}
+
+// todo: fix test case failure
+func Test_weightedQoSMBPolicy_getProportionalLossPlan(t *testing.T) {
+	t.Parallel()
+	type args struct {
+		loss        int
+		qosGroupMBs map[task.QoSLevel]*monitor.MBQoSGroup
+	}
+	tests := []struct {
+		name string
+		args args
+		want *plan.MBAlloc
+	}{
+		{
+			name: "weights inside",
+			args: args{
+				loss: 20_000,
+				qosGroupMBs: map[task.QoSLevel]*monitor.MBQoSGroup{
+					"foo": {
+						CCDMB: map[int]int{2: 5_000, 3: 5_000},
+						WeightedCCDMBs: map[int]map[int]int{
+							50: {2: 2000, 3: 1000},
+							16: {2: 1000, 6: 2000},
+							10: {2: 2000, 5: 2000},
+						},
+					},
+					"bar": {CCDMB: map[int]int{2: 5_000, 3: 5_000}},
+				},
+			},
+			want: &plan.MBAlloc{Plan: map[task.QoSLevel]map[int]int{
+				"foo_50": {2: 1615, 3: 807},
+				"foo_16": {2: 258, 6: 516},
+				"foo_10": {2: 323, 5: 323},
+				"bar":    {2: 8088, 3: 8080},
+			}},
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			w := &weightedQoSMBPolicy{}
+			assert.Equalf(t, tt.want, w.getProportionalLossPlan(tt.args.loss, tt.args.qosGroupMBs), "getProportionalLossPlan(%v, %v)", tt.args.loss, tt.args.qosGroupMBs)
 		})
 	}
 }
