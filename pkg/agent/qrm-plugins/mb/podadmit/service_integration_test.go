b169a9f01ea40b118880dfd47bb31babb0ac848e
Hongwei Chen <hongwei.chen@bytedance.com>
2024-11-03 17:58:26 +0000

    mbm: pod admitter service integration test using /tmp/qrm_mb_plugin.sock as ep

--- pkg/agent/qrm-plugins/mb/podadmit/service_integration_test.go
+++ pkg/agent/qrm-plugins/mb/podadmit/service_integration_test.go
@@ -21,44 +21,69 @@ package podadmit
 
 import (
 	"context"
-	"net"
-	"testing"
-
-	"github.com/stretchr/testify/assert"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/test/bufconn"
-
-	pluginapi "k8s.io/kubelet/pkg/apis/resourceplugin/v1alpha1"
-
+	"github.com/kubewharf/katalyst-core/pkg/agent/qrm-plugins/mb/controller"
 	"github.com/kubewharf/katalyst-core/pkg/agent/qrm-plugins/mb/controller/mbdomain"
 	"github.com/kubewharf/katalyst-core/pkg/config/generic"
+	"github.com/kubewharf/katalyst-core/pkg/util/process"
+	watcherapi "k8s.io/kubelet/pkg/apis/pluginregistration/v1"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	pluginapi "k8s.io/kubelet/pkg/apis/resourceplugin/v1alpha1"
 )
 
 func Test_Pod_Admit_Service_Integration(t *testing.T) {
 	t.Logf("lengthy integration test; not intended as part of check-in tests")
 
-	lis := bufconn.Listen(1024 * 1024)
-	s := grpc.NewServer()
-	manager := &admitter{
-		qosConfig:     generic.NewQoSConfiguration(),
-		domainManager: &mbdomain.MBDomainManager{},
+	////lis := bufconn.Listen(1024 * 1024)
+	//lis, err := net.Listen("unix", "/tmp/qrm_mb_plugin.sock")
+	//if err != nil {
+	//	t.Errorf("test error : %v", err)
+	//	return
+	//}
+	//s := grpc.NewServer()
+	//manager := &admitter{
+	//	qosConfig:     generic.NewQoSConfiguration(),
+	//	domainManager: &mbdomain.MBDomainManager{},
+	//}
+	//pluginapi.RegisterResourcePluginServer(s, manager)
+	//go func() {
+	//	if err := s.Serve(lis); err != nil {
+	//		t.Errorf("failed to serve: %v", err)
+	//	}
+	//}()
+	//
+	dummyQoSConfig := generic.NewQoSConfiguration()
+	dummyDomainManager := &mbdomain.MBDomainManager{}
+	stubController, _ := controller.New(nil, nil, dummyDomainManager, nil)
+	svc, err := NewPodAdmitService(dummyQoSConfig, dummyDomainManager, stubController, []string{"/tmp"})
+	if err != nil {
+		t.Errorf("test error : %v", err)
+		return
 	}
-	pluginapi.RegisterResourcePluginServer(s, manager)
-	go func() {
-		if err := s.Serve(lis); err != nil {
-			t.Errorf("failed to serve: %v", err)
-		}
-	}()
+	_ = svc.Start()
+	defer svc.Stop()
 
-	conn, err := grpc.DialContext(context.Background(), "bufnet", grpc.WithContextDialer(func(ctx context.Context, s string) (net.Conn, error) {
-		return lis.Dial()
-	}), grpc.WithInsecure())
+	//conn, err := grpc.DialContext(context.Background(), "bufnet", grpc.WithContextDialer(func(ctx context.Context, s string) (net.Conn, error) {
+	//	return lis.Dial()
+	//}), grpc.WithInsecure())
+	conn, err := process.Dial("/tmp/qrm_mb_plugin.sock", time.Second*3)
 	if err != nil {
 		t.Errorf("dial: %v", err)
+		return
 	}
 
+	regClient := watcherapi.NewRegistrationClient(conn)
+	r, err := regClient.GetInfo(context.TODO(), &watcherapi.InfoRequest{})
+	if err != nil {
+		t.Errorf("unexpected error %v", err)
+		return
+	}
+	t.Logf("registration: %v", r)
+
 	client := pluginapi.NewResourcePluginClient(conn)
-	resp, err := client.AllocateForPod(context.Background(), &pluginapi.PodResourceRequest{
+	resp, err := client.Allocate(context.Background(), &pluginapi.ResourceRequest{
 		PodUid:       "pod-123-4567",
 		PodNamespace: "ns-test",
 		PodName:      "pod-test",
