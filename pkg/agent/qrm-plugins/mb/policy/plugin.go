5768ba1888c116c2044b52098a10e6a1a759b223
Hongwei Chen <hongwei.chen@bytedance.com>
2025-08-28 22:01:00 +0000

    wip

--- pkg/agent/qrm-plugins/mb/policy/plugin.go
+++ pkg/agent/qrm-plugins/mb/policy/plugin.go
@@ -89,18 +89,21 @@ func (m *MBPlugin) run() {
 		general.Errorf("[mbm] failed to get mb data: %v", err)
 		return
 	}
+	general.InfofV(6, "[mbm] raw: group x ccd-level mb stat: %v", mbData)
 
 	statOutgoing := getGroupMonStat(mbData)
 	if mbData == nil {
 		general.Warningf("[mbm] got empty mb data")
 		return
 	}
+	general.InfofV(6, "[mbm] adapted: group x ccd-level mb stat: %v", statOutgoing)
 
 	monData, err := monitor.NewDomainStats(statOutgoing, m.ccdToDomain, m.xDomGroups)
 	if err != nil {
 		general.Errorf("[mbm] failed to run fetching mb stats: %v", err)
 		return
 	}
+	general.InfofV(6, "[mbm] mb domain summary mb stat: %v", statOutgoing)
 
 	ctx := context.Background()
 	var mbPlan *plan.MBPlan
@@ -127,6 +130,7 @@ func newMBPlugin(ccdMinMB, ccdMaxMB int, defaultDomainCapacity int,
 	planAllocator allocator.PlanAllocator, emitPool metricspool.MetricsEmitterPool,
 ) skeleton.GenericPlugin {
 	ccdMappings := domains.GetCCDMapping()
+	general.Infof("[mbm] initialization: ccd-to-domain mapping %v", ccdMappings)
 	emitter := emitPool.GetDefaultMetricsEmitter().WithTags(metricName)
 	return &MBPlugin{
 		emitter:     emitter,
