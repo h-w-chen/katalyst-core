81263a87247e1f543f8057b9cc94e092545d23d6
Hongwei Chen <hongwei.chen@bytedance.com>
2025-11-28 21:36:25 +0000

    wip: license fmt fix

--- pkg/agent/sysadvisor/plugin/qosaware/resource/cpu/assembler/headroomassembler/decorator/decorator.go
+++ pkg/agent/sysadvisor/plugin/qosaware/resource/cpu/assembler/headroomassembler/decorator/decorator.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2022 The Katalyst Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package decorator
 
 import (
@@ -10,9 +26,11 @@ import (
 	"github.com/kubewharf/katalyst-core/pkg/metrics"
 )
 
-var initializers sync.Map
-var enabledDecorator string
-var lock sync.RWMutex
+var (
+	initializers     sync.Map
+	enabledDecorator string
+	lock             sync.RWMutex
+)
 
 type InitFunc func(inner headroomassembler.HeadroomAssembler,
 	conf *config.Configuration, extraConf interface{},
